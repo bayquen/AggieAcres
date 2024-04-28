966b7de187fc2b9c75073d13c7f6bed15079027d
Wuyudaxia <1879233041@qq.com>
2024-04-27 19:27:08 -0700

    Upload Spring

---
+++ frontend/AggieFront/src/main/java/com/happyaggies/aggiefront/AggieFrontApplication.java
@@ -0,0 +1,13 @@
+package com.happyaggies.aggiefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggieFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggieFrontApplication.class, args);
+    }
+
+}
---
+++ frontend/AggieFront/src/test/java/com/happyaggies/aggiefront/AggieFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyaggies.aggiefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggieFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
